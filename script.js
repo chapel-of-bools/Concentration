b23f85db8499e822aea7a504b84c1d3a1cda8e3b
Matthew Scott <matthewruelscott@gmail.com>
2016-06-06 15:46:26 -0400

    adds images

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var cardArray = ['A','A','B','B','C','C','D','D','E','E','F','F'];
+// var cardArray = ['A','A','B','B','C','C','D','D','E','E','F','F'];
+var cardArray = ['bt.jpg','bt.jpg','btilc.jpg','btilc.jpg','dp.jpg','dp.jpg','efny.jpg','efny.jpg','h8.jpg','h8.jpg','tt.jpg','tt.jpg']
 var cardValues = [];
 var cardIds = [];
 var cardsFlipped = 0;
@@ -18,15 +19,15 @@ function newBoard(){
   var output = " ";
   cardArray.shuffle();
   for(var i = 0; i < cardArray.length; i++){
-    output += '<div id="card_'+i+'" onclick="flipCard(this,\''+cardArray[i]+'\')"></div>';
+    // output += '<div id="card_'+i+'" onclick="flipCard(this,\''+cardArray[i]+'\')"></div>';
+    output += '<div id="card_'+i+'"><img style="opacity:0" src="'+cardArray[i]+'" onclick="flipCard(this)" /></div>';
     $("#board").html(output);
   }
 }
 // Card Flipping
 function flipCard(card,val){
-  if(card.innerHTML == "" && cardValues.length < 2){
-    card.style.background = "white";
-    card.innerHTML = val;
+  if(card.style.opacity == 0 && cardValues.length < 2){
+    card.style.opacity = 1;
     if(cardValues.length == 0){
       cardValues.push(val);
       cardIds.push(card.id);
@@ -38,7 +39,7 @@ function flipCard(card,val){
         // Clear both arrays
         cardValues = [];
         cardIds = [];
-        // Check to see if the whole board is cleared
+        // Check to see board is cleared
         if(cardsFlipped == cardArray.length){
           alert("You win! Let's play again!");
           $("#board").html("");
